f9e3255e33f31faa32bad90ba017c2a737e1bef8
igor.tsupko <igor.tsupko@flant.com>
2018-07-14 19:51:01 +0300

    Сделали докер

--- python/app.py
+++ python/app.py
@@ -1,10 +1,35 @@
+# coding=utf-8
 from flask import Flask
 import json
 app = Flask(__name__)
 
 
+@app.route('/')
+def root():
+    return "please send requests to API only"
+
 @app.route('/api/projects')
 def list_projects():
+    """
+    :return: json со списком проектов
+
+    **Response:**
+    ```
+    {
+        "list": [
+            {
+                "name": "MyProject1",
+                "remote": "git@mywebsite.ru:myproject1",
+            }
+        ],
+        "_meta": {
+            "per-page": 12,
+            "page": 12,
+            "total-pages": 12
+        }
+    }
+    ```
+    """
     response = {
         "list": [
             {
@@ -21,7 +46,19 @@ def list_projects():
     return json.dumps(response)
 
 @app.route('/api/projects/<id>')
-def hello_name(id):
+def get_project(id):
+    """
+    :param id: идентификатор проекта
+    :return: json с описанием объекта
+
+    **Response:**
+    ```
+    {
+        "name": "MyProject1",
+        "remote": "git@mywebsite.ru:myproject1",
+    }
+    ```
+    """
     response = {
         "id": id,
         "name": "MyProject1",
@@ -31,4 +68,4 @@ def hello_name(id):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
