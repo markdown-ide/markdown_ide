6b47453b9e09be421be03eb021e6420ed4a1f2cf
igor.tsupko <igor.tsupko@flant.com>
2018-07-14 19:09:49 +0300

    заготовки двух методов

--- python/app.py
+++ python/app.py
@@ -1,10 +1,34 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/api/projects')
+def list_projects():
+    response = {
+        "list": [
+            {
+                "name": "MyProject1",
+                "remote": "git@mywebsite.ru:myproject1",
+            }
+        ],
+        "_meta": {
+            "per-page": 12,
+            "page": 12,
+            "total-pages": 12
+        }
+    }
+    return json.dumps(response)
+
+@app.route('/api/projects/<id>')
+def hello_name(id):
+    response = {
+        "id": id,
+        "name": "MyProject1",
+        "remote": "git@mywebsite.ru:myproject1",
+    }
+    return json.dumps(response)
+
 
 if __name__ == '__main__':
     app.run()
